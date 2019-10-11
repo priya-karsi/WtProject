6e47f876a370feba2227a7a53535077ef502ba02
priya karsi <priya.karsi11@gmail.com>
2019-10-11 16:17:14 +0530

    make schedule final

--- resources/views/pages/adminschedule.blade.php
+++ resources/views/pages/adminschedule.blade.php
@@ -21,7 +21,14 @@
          background-color: black;
          color:burlywood;
     }
+*/
+.d{
+    font-family:'Times New Roman', Times, serif; 
+    color:#566f90;
+    font-size:20px;
 
+}
+/*
     .d2{
         text-align: center;
          font-family:'Times New Roman', Times, serif; 
@@ -49,15 +56,15 @@
                         {{-- </div> --}}
                         <div class="card-body">
                                 <div class="form-group row">
-                                        <label for="date" class="col-md-4 col-form-label text-md-right d2">{{ __('Select Standard') }} </label>
+                                        <label for="date" class="col-md-4 col-form-label text-md-right d">{{ __('Select Standard :') }} </label>
                                             </button>
                             
                                         <div class="col-md-6">
-                                                <div class="form-group" style="text-align:center;" >
-                                                        <select class=" form-control d1"   name="standard" id="standard">
-                                                                <option class="d1" value="8">Standard VIII</option>
-                                                                <option  class="d1" value="9">Standard IX</option>
-                                                                <option class="d1" value="10">Standard X</option>
+                                                <div class="form-group " style="text-align:center;" >
+                                                        <select class=" form-control d"   name="standard" id="standard">
+                                                                <option class="d" value="8">Standard VIII</option>
+                                                                <option  class="d" value="9">Standard IX</option>
+                                                                <option class="d" value="10">Standard X</option>
                                                         </select>
                                                 
                                                     {{-- <label for="date" class="col-md-2 col-form-label text-md-right">{{ __('Date') }} </label>
@@ -70,49 +77,43 @@
                                 </div>
                                
             <div class="form-group row">
-                    <label for="date" class="col-sm-4 col-md-4 col-form-label text-md-right d2">{{ __('Date') }} </label>
+                    <label for="date" class="col-sm-4 col-md-4 col-form-label text-md-right d">{{ __('Date :') }} </label>
                         </button>
         
                     <div class="col-sm-4 col-md-6">
-                        <input id="date" type="date" class="form-control d2" name="date" required autocomplete="date"> 
+                        <input id="date" type="date" class="form-control d" name="date" required autocomplete="date"> 
                     </div>
         
             </div>
             <div class="form-group row">
-                    <label for="time_in" class="col-sm-4 col-md-4 col-form-label text-md-right d2">{{ __('Time in') }}</label>
-        
-                    <div class="col-sm-6 col-md-6">
-                        <input id="time_in" type="time" class="form-control d2" name="time_in" required autocomplete="time_in">
-                    </div>
-            </div>
-            <div class="form-group row">
-                    <label for="time_out" class="col-sm-4 col-md-4 col-form-label text-md-right d2">{{ __('Time out') }}</label>
+                    <label for="time_in" class="col-sm-4 col-md-4 col-form-label text-md-right d">{{ __('Time in :') }}</label>
         
                     <div class="col-sm-6 col-md-6">
-                        <input id="time_out" type="time" class="form-control d2" name="time_out" required autocomplete="time_out">
+                        <input id="time_in" type="time" class="form-control d" name="time_in" required autocomplete="time_in">
                     </div>
             </div>
             <div class="form-group row">
-                <label for="no_lecs" class="col-sm-4 col-md-4 col-form-label text-md-right d2">{{ __('Enter the number of lectures :') }}</label>
+                    <label for="time_out" class="col-sm-4 col-md-4 col-form-label text-md-right d">{{ __('Time out :') }}</label>
+        
+                    <div class="col-sm-6 col-md-6">
+                        <input id="time_out" type="time" class="form-control d" name="time_out" required autocomplete="time_out">
+                    </div>
+            </div>
+            <div class="form-group row">
+                <label for="no_lecs" class="col-sm-4 col-md-4 col-form-label text-md-right d">{{ __('Enter the number of lectures :') }}</label>
         
                 <div class="col-sm-6 col-md-6">
-                    <input id="no_lecs" class="form-control d2" name="no_lecs" required autocomplete="no_lecs" value="0" min="0" max="4" type="number" />
+                    <input id="no_lecs" class="form-control d" name="no_lecs" required autocomplete="no_lecs" value="0" min="0" max="4" type="number" />
                 </div>
         </div>
         <div class="container">
             <div class="row">
                 <div class="col-md-6 offset-md-4">
-            <button style="" id="show" type="button" class="btn btn-default d2">
-                {{ __('Show details') }}
+            <button style="border: 1px solid darkblue;" id="show" type="button" class="btn btn-default d">
+                {{ __('Show details =>') }}
             </button>
         </div>
-            <div class="form-group row mb-0">
-                    <div class="col-md-6 offset-md-4">
-                        <button type="submit" class="btn btn-default d2">
-                            {{ __('Submit') }}
-                        </button>
-                    </div>
-            </div>
+            
             <br><br><br><br>
         </div>
         </div>
@@ -124,6 +125,13 @@
         
                 
         
+</div>
+<div id="hs" class="form-group row mb-0">
+        <div class="col-md-6">
+            <button style="border: 1px solid darkblue;  font-weight:bolder;" type="submit" class="btn btn-default d">
+                {{ __('Make Schedule!') }}
+            </button>
+        </div>
 </div>
 
     
@@ -137,6 +145,7 @@
 </div>
     <script>
         $(document).ready(function() {
+        $("#hs").hide();
         $("#show").click(function(){
             var no = $("#no_lecs").val();
             console.log(no);
@@ -149,7 +158,7 @@
 let createTaskCard = () => {
 
     let card = document.createElement('div');
-    card.className = 'card shadow cursor-pointer';
+    card.className = 'card bg-light';
 
     let cardBody = document.createElement('div');
     cardBody.className = 'card-body';
@@ -181,8 +190,7 @@ let createTaskCard = () => {
                 l2.setAttribute("for","time_out_"+i);
                 l2.setAttribute("class"," col-form-label text-md-right");
                 l2.innerHTML="Select Time out "+i+": ";
-                //l2.className = 'c3';
-                //document.getElementById("card-container").appendChild(z);
+                //l2.clant.getElementById("card-container").appendChild(z);
 
 
     let i2 = document.createElement('INPUT');
@@ -190,13 +198,21 @@ let createTaskCard = () => {
     i2.setAttribute("name","time_out_"+i);
     i2.setAttribute("class","col-md-8 form-control");
     //i2.className = 'c4';
-    //document.getElementById("card-container").appendChild(y);
+    //document.getElssName = 'c3';
+                //documeementById("card-container").appendChild(y);
 
     let div3 = document.createElement('div');
+
+    let l3 = document.createElement('LABEL');
+                l3.setAttribute("for","select_teacher_"+i);
+                l3.setAttribute("class"," col-form-label text-md-right");
+                l3.innerHTML="By : ";
+                //l2.clant.getElementById("card-container").appendChild(z);
+
     let s = document.createElement('SELECT');
                 s.setAttribute("name","teacher_"+i);
                 s.setAttribute("class","down form-control");
-                s.setAttribute("style","margin-top:10px;");
+                s.setAttribute("style","width:100px; margin-top:0px;");
                 s.setAttribute("id","select_"+i);
                 document.getElementById("card-container").appendChild(s);
 
@@ -265,6 +281,7 @@ let createTaskCard = () => {
     div2.appendChild(l2);
     div2.appendChild(i2);
     cardBody.appendChild(div3);
+    div3.appendChild(l3);
     div3.appendChild(s);
     card.appendChild(cardBody);
     cardContainer.appendChild(card);
@@ -285,6 +302,7 @@ let initListOfTasks = () => {
 
 initListOfTasks();
 }
+$("#hs").show();
 });
         
     });
